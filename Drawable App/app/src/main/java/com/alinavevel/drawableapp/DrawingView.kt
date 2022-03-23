ca0cb6fa2a9a63264210188247e3f49bfd93fa50
Alina <alinavevel@gmail.com>
2022-03-23 16:34:44 +0100

    Adding size brush and color choice

--- Drawable App/app/src/main/java/com/alinavevel/drawableapp/DrawingView.kt
+++ Drawable App/app/src/main/java/com/alinavevel/drawableapp/DrawingView.kt
@@ -3,6 +3,8 @@ package com.alinavevel.drawableapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context : Context, attribute : AttributeSet) : View(context, attribute) {
@@ -14,6 +16,7 @@ class DrawingView(context : Context, attribute : AttributeSet) : View(context, a
     private var mCanvasPaint : Paint? = null
     private var mCanvasBitmap : Bitmap? = null
     private var canvas : Canvas? = null
+    private val mPaths = ArrayList<CustomPatch>() // ArrayList for Paths
 
     init {
         setUpVar()
@@ -34,6 +37,76 @@ class DrawingView(context : Context, attribute : AttributeSet) : View(context, a
         mSizeBrush = 20.toFloat()
     }
 
+    fun setColor(newColor: String) {
+        color = Color.parseColor(newColor)
+        mDrawPaint?.color = color
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for (p in mPaths) {
+            mDrawPaint?.strokeWidth = p.bruchTick
+            mDrawPaint?.color = p.color
+            canvas.drawPath(p, mDrawPaint!!)
+        }
+
+        if(!mDrawPath!!.isEmpty){
+            mDrawPaint!!.strokeWidth = mDrawPath!!.bruchTick
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mSizeBrush = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint?.strokeWidth = mSizeBrush
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath?.color = color
+                mDrawPath?.bruchTick = mSizeBrush
+
+                mDrawPath?.reset() // Clear any lines and curves from the path, making it empty.
+                mDrawPath?.moveTo(
+                    touchX!!,
+                    touchY!!
+                )
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if(touchX != null){
+                    if(touchY != null){
+
+                        mDrawPath!!.lineTo(touchX, touchY)
+                    }
+                }
+            }
+
+            MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
+                mDrawPath = CustomPatch(color, mSizeBrush)
+            }
+            else -> return false
+
+
+        }
+        invalidate()
+
+
+        return true
+    }
+
 
 
     internal inner class CustomPatch( var color : Int, var bruchTick : Float) : Path(){
@@ -44,6 +117,8 @@ class DrawingView(context : Context, attribute : AttributeSet) : View(context, a
         super.onSizeChanged(w, h, oldw, oldh)
 
         mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+
+        canvas = Canvas(mCanvasBitmap!!)
     }
 
 
--- Drawable App/app/src/main/java/com/alinavevel/drawableapp/MainActivity.kt
+++ Drawable App/app/src/main/java/com/alinavevel/drawableapp/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.alinavevel.drawableapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.alinavevel.drawableapp.databinding.ActivityMainBinding
+import com.alinavevel.drawableapp.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.drawView.setSizeForBrush(20.toFloat())
+
+        binding.buttonBrush.setOnClickListener {
+            showBrushDialog()
+        }
+
+        binding.btnBrown.setOnClickListener {
+            binding.drawView.setColor("#8F442C")
+        }
+    }
+
+    private fun showBrushDialog(){
+        var brushDialog = Dialog(this)
+        brushDialog.setTitle("Brush size: ")
+        var bindingDialog : DialogBrushSizeBinding = DialogBrushSizeBinding.inflate(LayoutInflater.from(this))
+        brushDialog.setContentView(bindingDialog.root)
+        bindingDialog.small.setOnClickListener {
+            binding.drawView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        bindingDialog.medium.setOnClickListener {
+            binding.drawView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        bindingDialog.large.setOnClickListener {
+            binding.drawView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+
+
+
+
+
+
+
+        }
+
+        brushDialog.show()
+
+
     }
 }
